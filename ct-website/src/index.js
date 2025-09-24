32fbd9e4aa155a4d2ec38a339f136f5a7b716567
CTC3PO <chuinus@gmail.com>
2025-09-24 21:27:06 +0700

    add project structure and files

---
+++ ct-website/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/globals.css';  
+import Portfolio from './Portfolio.jsx';  
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+);
